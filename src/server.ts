4d208cd1788e741c0e46618417e3da96c584be75
Mitch Allen <github@mitchallen.com>
2024-01-16 06:18:38 -0500

    feat: updated root response

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,17 @@ import * as express from 'express';
 
 const app = express();
 
+// may require npm start launch to work (which we do)
+const VERSION = process.env.npm_package_version;
+const API_TITLE = process.env.npm_package_name;
+
 app.get('/', (req, res) => {
-  res.send('Hello, Express TypeScript!');
+  const record = {
+    name: API_TITLE,
+    version: VERSION,
+    author: 'Mitch Allen',
+  }
+  res.status(200).json(record);
 });
 
 app.listen(3000, () => {
